46d50c467aad3bbc179f3d20cf62870bdc3c645a
FQ <1794682729@.qq.com>
2025-11-09 21:29:57 +0800

    基于互斥锁解决缓存击穿问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,7 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
@@ -14,8 +16,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -31,24 +32,90 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     private StringRedisTemplate stringRedisTemplate;
     @Override
     public Result selectById(Long id) {
+        //防止缓存穿透
+//        Shop shop = queryWithPassThrough(id);
+        //缓存击穿
+        Shop shop = getCacheWithMutexForHotKey(id);
+        if (shop == null) {
+            Result.fail("商户不存在");
+        }
+        return Result.ok(shop);
+
+    }
+    //防止缓存击穿
+    public Shop getCacheWithMutexForHotKey(Long id) {
+        Shop shop=null;
         String key=CACHE_SHOP_KEY+id;
         //从Redis中查询是否存在商户信息
         String shopJson = stringRedisTemplate.opsForValue().get(key);
-        if (shopJson!=null) {
+        if (StrUtil.isNotBlank(shopJson)) {
+            //如果命中则直接返回数据
+             shop = JSONUtil.toBean(shopJson, Shop.class);
+            return shop;
+        }
+        //判断是否命中的是空值，经过上一轮判断能到这个if的要么是null,要么是"";
+        if (shopJson!=null){
+            //返回错误信息
+            return null;
+        }
+        String lockKey="shop:lock:"+id;
+        Boolean lock = tryLock(lockKey);
+        try {
+
+            if (!lock) {
+                Thread.sleep(50);
+                return getCacheWithMutexForHotKey(id);
+            }
+            shop = getById(id);
+            //模拟重建的时间
+            Thread.sleep(200);
+            if (shop == null) {
+                stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
+                return null;
+            }
+            stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), 30, TimeUnit.SECONDS);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        finally {
+            unlock(lockKey);
+        }
+        return shop;
+    }
+
+    public  Shop queryWithPassThrough(Long id) {
+        String key=CACHE_SHOP_KEY+id;
+        //从Redis中查询是否存在商户信息
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        if (StrUtil.isNotBlank(shopJson)) {
             //如果命中则直接返回数据
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return shop;
+        }
+        //判断是否命中的是空值，经过上一轮判断能到这个if的要么是null,要么是"";
+        if (shopJson!=null){
+            //返回错误信息
+            return null;
         }
         //如果未命中则根据id查询数据库中的数据
         Shop shop = getById(id);
         //判断该用户是否存在，如果存在则将数据存入redis中，如果不存在直接返回不存在
         if (shop==null) {
+
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
             Result.fail("该商户不存在");
         }
         stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
-        //返回商铺信息
-        return Result.ok(shop);
-
+        return shop;
+    }
+    //获取锁
+    public Boolean tryLock(String key) {
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.MINUTES);
+        return BooleanUtil.isTrue(flag);
+    }
+    //释放锁
+    public void unlock(String key) {
+        stringRedisTemplate.delete(key);
     }
 
     @Override
@@ -56,6 +123,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
     public Result update(Shop shop) {
         Long id = shop.getId();
         if (id==null){
+
             return Result.fail("id不存在");
         }
         String key=CACHE_SHOP_KEY+id;
